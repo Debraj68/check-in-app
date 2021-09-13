58ed7f60d158a799b1d03fab5151aeceba0b1462
Debraj68 <mukherjee.debraj861@outlook.com>
2021-09-13 16:19:16 +0530

    FCI-4 another changes to a filr

--- src/main/java/com/myapp/spring/CheckInFlightApplication.java
+++ src/main/java/com/myapp/spring/CheckInFlightApplication.java
@@ -3,11 +3,11 @@ package com.myapp.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class CheckInFlightApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(CheckInFlightApplication.class, args);
-//	}
-//
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckInFlightApplication.class, args);
+	}
+
 }
